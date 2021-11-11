b6e1d196926d6e36be1509f121e36a2c5290704d
Nikita Lebedenko <nlebn001@gmail.com>
2021-11-11 22:19:16 +0100

    Task3 Added

--- EssentialLesson10Task3/MyDictionary.cs
+++ EssentialLesson10Task3/MyDictionary.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,118 @@ using System.Threading.Tasks;
 
 namespace EssentialLesson10Task3
 {
-    class MyDictionary <TKey, TValue>
+    class MyDictionary<TKey, TValue> 
     {
 
+        TKey[] keyArr = new TKey[0];
+        TValue[] valueArr = new TValue[0];
 
+        public int Count => keyArr.Length;
+        public (TKey, TValue) this[int index] => (index > keyArr.Length || index < 0) ? throw new IndexOutOfRangeException($"Impossible to get value by index {index}") :
+                                                                                        (keyArr[index], valueArr[index]);
+
+        public void Add(TKey key, TValue value)
+        {
+            if (key == null || value == null) throw new NullReferenceException("Incoming parameter cant be NULL");
+
+            for (int i = 0; i < keyArr.Length; i++)
+            {
+                if (key.Equals(keyArr[i]))
+                {
+                    valueArr[i] = value;
+                    return;
+                }
+            }
+
+            Array.Resize(ref keyArr, keyArr.Length + 1);
+            Array.Resize(ref valueArr, valueArr.Length + 1);
+            keyArr[keyArr.Length - 1] = key;
+            valueArr[valueArr.Length - 1] = value;
+        }
+
+        public (TKey, TValue) GetPairByValue(TValue value)
+        {
+            TKey retKey = default;
+            TValue retValue = default;
+
+            if (value == null) throw new NullReferenceException("Incoming value was null");
+
+            for (int i = 0; i < valueArr.Length; i++)
+            {
+                if (value.Equals(valueArr[i]))
+                {
+                    retKey = keyArr[i];
+                    retValue = valueArr[i];
+                    break;
+                }
+                else
+                {
+                    throw new Exception("COllection doesnt have this value");
+                }
+            }
+            return (retKey, retValue);
+        }
+
+        public (TKey, TValue) GetPairByKey(TKey key)
+        {
+            TKey retKey = default;
+            TValue retValue = default;
+
+            if (key == null) throw new NullReferenceException("Incoming value was null");
+
+            for (int i = 0; i < keyArr.Length; i++)
+            {
+                if (key.Equals(keyArr[i]))
+                {
+                    retKey = keyArr[i];
+                    retValue = valueArr[i];
+                    break;
+                }
+                else
+                {
+                    throw new Exception("COllection doesnt have this value");
+                }
+            }
+            return (retKey, retValue);
+        }
+
+        public void DeleteByKey(TKey key)
+        {
+            int temp = default;
+
+            if (key == null) throw new NullReferenceException("Incoming value was null");
+
+            for (int i = 0; i < keyArr.Length; i++)
+            {
+                if (i == keyArr.Length - 1)
+                {
+                    break;
+                }
+                else if (key.Equals(keyArr[i]))
+                {
+                    temp = i;
+                    break;
+                }
+                else
+                {
+                    throw new Exception("COllection doesnt have this key");
+                }
+            }
+
+            for (int i = temp; i < keyArr.Length; i++)
+            {
+                if (i == keyArr.Length - 1)
+                {
+                    break;
+                }
+                keyArr[i] = keyArr[i + 1];
+                valueArr[i] = valueArr[i + 1];
+            }
+
+            Array.Resize(ref keyArr, keyArr.Length - 1);
+            Array.Resize(ref valueArr, valueArr.Length - 1);
+
+        }
 
     }
 }
--- EssentialLesson10Task3/Program.cs
+++ EssentialLesson10Task3/Program.cs
@@ -6,7 +6,24 @@ namespace EssentialLesson10Task3
     {
         static void Main(string[] args)
         {
-            
+            MyDictionary<string, string> test = new MyDictionary<string, string>();
+
+            test.Add("Odin", "One");
+            test.Add("Dva", "Two");
+            test.Add("Tri", "Three");
+            test.Add("Chetire", "Four");
+            //test.DeleteByKey("Odin");
+
+            Console.WriteLine(test[0]);
+            Console.WriteLine(test[1]);
+            Console.WriteLine(test[2]);
+
+            Console.WriteLine(test.GetPairByKey("Odin"));
+
+            Console.WriteLine(test.GetPairByValue("One"));
+
+            Console.WriteLine(test.Count);
+
         }
     }
 }
